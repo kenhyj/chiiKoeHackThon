2d5072bcc7177c3bd352532e47c692eed946da16
kenhyj <mathkenjeong@gmail.com>
2022-01-31 00:56:29 -0800

    styled balck white grey stars possible. next on the list. handle changes upon click

--- q2/q2/component/Stars.jsx
+++ q2/q2/component/Stars.jsx
@@ -5,45 +5,55 @@ import IconButton from '@mui/material/IconButton';
 import StarTwoToneIcon from '@mui/icons-material/StarTwoTone';
 import StarBorderTwoToneIcon from '@mui/icons-material/StarBorderTwoTone';
 import StarIcon from '@mui/icons-material/Star';
-import styles from "./Stars.module.css";
+
 import { grey } from '@mui/material/colors';
 
 import Rating from '@mui/material/Rating';
+
+import { styled } from '@mui/material/styles';
+
+const StyledRating = styled(Rating)({
+    '& .MuiRating-iconFilled': {
+      color: "black",
+    },
+    '& .MuiRating-iconHover': {
+      color: grey[500],
+    },
+  });
 
 
 import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
 
 const theme = createTheme({
-    overrides: {
-        MuiIconButton: {
-            root: {
-                '&:hover': {
-                    backgroundColor: "$labelcolor"
-                }
-            }
-        }
-    }
-})
+    components: {
+      // Name of the component
+      MuiRating: {
+        styleOverrides: {
+          // Name of the slot
+          iconFilled: {color: "green"},
+          iconEmpty: {color: "white"},
+        },
+      },
+    },
+  });
 
 
 export default function Stars(props) {
     const blackStar = [...Array(props.stars)].map((e, i) => <IconButton> <StarIcon style={{ color: "black" }} theme={theme} /> </IconButton>);
     const whiteStar = [...Array(5 - props.stars)].map((e, i) => <IconButton > <StarBorderTwoToneIcon /> </IconButton>);
 
-    // const greyStar = [...Array(props.stars)].map((e, i) => <IconButton> <StarIcon style={{color: grey[500] }} theme={theme}/> </IconButton>);
+    const greyStar = [...Array(props.stars)].map((e, i) => <IconButton> <StarIcon style={{color: grey[500] }} theme={theme}/> </IconButton>);
 
     // const oneLineStar = [...Array(5)].map((e, i) => props.stars > i ? <StarIcon />: <StarBorderTwoToneIcon/>);
     return (
         <div>
-            <Rating
+            <StyledRating
                 value={props.stars}
                 precision={0.5}
-                iconFilled= {{color: "black"}}
             />
             {/*
             {blackStar}
             {whiteStar} */}
         </div>
-        // <StarTwoToneIcon />
     )
 }
