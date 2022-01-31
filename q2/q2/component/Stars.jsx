0dee7bb4c1cd793d59fa2bab0533cfa62ddf7c26
kenhyj <mathkenjeong@gmail.com>
2022-01-31 00:32:39 -0800

    mui rating might be a lot easier to use

--- q2/q2/component/Stars.jsx
+++ q2/q2/component/Stars.jsx
@@ -6,16 +6,43 @@ import StarTwoToneIcon from '@mui/icons-material/StarTwoTone';
 import StarBorderTwoToneIcon from '@mui/icons-material/StarBorderTwoTone';
 import StarIcon from '@mui/icons-material/Star';
 import styles from "./Stars.module.css";
+import { grey } from '@mui/material/colors';
+
+import Rating from '@mui/material/Rating';
+
+
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+    overrides: {
+        MuiIconButton: {
+            root: {
+                '&:hover': {
+                    backgroundColor: "$labelcolor"
+                }
+            }
+        }
+    }
+})
+
 
 export default function Stars(props) {
-    const blackStar = [...Array(props.stars)].map((e, i) => <IconButton > <StarIcon/> </IconButton>);
-    const whiteStar = [...Array(5-props.stars)].map((e, i) => <IconButton > <StarBorderTwoToneIcon/> </IconButton>);
+    const blackStar = [...Array(props.stars)].map((e, i) => <IconButton> <StarIcon style={{ color: "black" }} theme={theme} /> </IconButton>);
+    const whiteStar = [...Array(5 - props.stars)].map((e, i) => <IconButton > <StarBorderTwoToneIcon /> </IconButton>);
+
+    // const greyStar = [...Array(props.stars)].map((e, i) => <IconButton> <StarIcon style={{color: grey[500] }} theme={theme}/> </IconButton>);
 
     // const oneLineStar = [...Array(5)].map((e, i) => props.stars > i ? <StarIcon />: <StarBorderTwoToneIcon/>);
     return (
         <div>
+            <Rating
+                value={props.stars}
+                precision={0.5}
+                iconFilled= {{color: "black"}}
+            />
+            {/*
             {blackStar}
-            {whiteStar}
+            {whiteStar} */}
         </div>
         // <StarTwoToneIcon />
     )
