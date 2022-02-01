aafafb9206633178ff08fbab9deeaaeabb4aad01
kenhyj <mathkenjeong@gmail.com>
2022-01-31 17:31:23 -0800

    a little confusing. need to clean up

--- q1/q1.py
+++ q1/q1.py
@@ -1,10 +1,11 @@
 #  https://towardsdatascience.com/do-you-know-python-has-a-built-in-database-d553989c87bd
+from multiprocessing.connection import Connection
 import sqlite3 as sl
 import sys
 
 # CREATES TABLE BOOK 
 
-def create_table_book(con):
+def create_table_book(con: Connection):
     with con: con.execute("""
     CREATE TABLE IF NOT EXISTS BOOK (
         id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -19,7 +20,7 @@ def create_table_book(con):
 # please note that "same books" but of different languages still have different isbn. 
 # eg. harry potter english vs harry potter chinese
 
-def create_book_entries(con):
+def create_book_entries(con: Connection):
     sql = "INSERT INTO BOOK(isbn, title, author, language) values (?, ?, ?, ?)"
     data = [
         ("0000000000", "Alice in Wonderland",  "Alice", "Aboriginal Canadian"),
@@ -41,16 +42,19 @@ def check_isbn_format(isbn: str):
     return ( 10 == len(isbn_clean) or 13 == len(isbn_clean) ) and (isbn_clean.isdecimal())
 
 
-def get_book_info (con, isbn: str):
+def get_book_info (con: Connection, isbn: str):
     """
     """
     if (check_isbn_format(isbn)):
         isbn_clean = isbn.strip().replace(" ", "").replace("-", "")
+        
         # call the DB
         with con: 
             data= con.execute("SELECT * FROM BOOK WHERE isbn = ?", [isbn_clean])
             for row in data:
+                book = row
                 print(row)
+        return book
     else:
         print("Please check your isbn format")
         isbn_clean = isbn.strip().replace(" ", "").replace("-", "")
@@ -59,7 +63,10 @@ def get_book_info (con, isbn: str):
         if (10 == len(isbn_clean) or 13 == len(isbn_clean)):
             print("your isbn has to include 10 or 13 numeric characters")
 
-def store_N_books (n = 1 ): 
+memoryBook = {}
+
+# decorator
+def store_books ( func ): 
     """
     Write a wrapper function that increase performance by keeping results
     in memory for the quick look up. To prevent memory from growing unbounded,
@@ -68,11 +75,17 @@ def store_N_books (n = 1 ):
     Assume that N can be a large number when choosing data structure(s) and
     algorithm(s)
     """
-    if (n < 1):
-        print ("store at least 1 book")
-        return
-        
-    print ("brb")
+    # wrapper
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        if len(memoryBook) > 100: # N = 100 here
+            # https://stackoverflow.com/questions/1756992/how-to-remove-the-oldest-element-from-a-dictionary
+            memoryBook.pop(next(iter(memoryBook)))
+    return wrapper
+
+@store_books    
+def store( book ):
+    memoryBook.update( {book[1]: book } )
 
 def main ():
     con = sl.connect('q1.db')
@@ -81,8 +94,11 @@ def main ():
         create_book_entries(con)
         print ("created some dummy entries for books")
     else:
-        get_book_info(con, sys.argv[1])
-    # print("testing 1 2 ")
+        book=get_book_info(con, sys.argv[1])
+
+    store(book) # 
+    for x in memoryBook:
+        print (x)
 
 
 main()
